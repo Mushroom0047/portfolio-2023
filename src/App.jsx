e756defd236ff4a494b89547b495e6424b5fabea
Mushroom <hectorvaldesm47@gmail.com>
2023-10-31 20:38:23 -0300

    Trying to fix the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { PrivacyPolicyUpperLower } from "./Pages/PrivacyPolicyUpperLower";
+import { NoPage } from "./Pages/NoPage";
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="privacy-policy-upper-lower" element={<PrivacyPolicyUpperLower />}/>
+        <Route path="*" element={<NoPage />}/>              
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Pages/NoPage.jsx
@@ -0,0 +1,6 @@
+
+export const NoPage = () => {
+  return (
+    <h1 className="text-center">404</h1>
+  )
+}
