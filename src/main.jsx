2cfd0774d69d1eeb8b72bd8675adff984b133b73
Hector Valdes <contacto@hectorvaldesm.com>
2023-11-01 00:03:25 -0300

    Router another try

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Home } from './Pages/Home';
 import { PrivacyPolicyUpperLower } from './Pages/PrivacyPolicyUpperLower';
 import { NoPage } from './Pages/NoPage';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/privacy-policy-upper-lower",
+    element: <PrivacyPolicyUpperLower />
+  },
+  {
+    path: "*",
+    element: <NoPage />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/privacy-policy-upper-lower" component={PrivacyPolicyUpperLower} />
-        <Route path="*" component={NoPage} />
-      </Switch>
-    </Router>
+    <RouterProvider router={router}/>
   </React.StrictMode>
-);
+)
