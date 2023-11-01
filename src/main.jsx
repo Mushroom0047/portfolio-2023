f4ddc5943d150b402d109cd2cb1a4ac9369724b3
Hector Valdes <contacto@hectorvaldesm.com>
2023-10-31 22:05:05 -0300

    Fixing 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { PrivacyPolicyUpperLower } from './Pages/PrivacyPolicyUpperLower';
 import { NoPage } from './Pages/NoPage';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "privacy-policy-upper-lower",
-    element: <PrivacyPolicyUpperLower />
-  },
-  {
-    path: "*",
-    element: <NoPage />
-  }
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/privacy-policy-upper-lower" component={PrivacyPolicyUpperLower} />
+        <Route path="*" component={NoPage} />
+      </Switch>
+    </Router>
   </React.StrictMode>
-)
+);
